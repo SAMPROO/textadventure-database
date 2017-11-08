db8948f63713aac01bc635b1b3a8fb09db40c5cf
SAMPROO <sampo.tuisku@gmail.com>
2017-11-08 23:39:42 +0200

    Npc interaction protype implemented
    npc interaction trial

--- textadventure-python/game.py
+++ textadventure-python/game.py
@@ -1,4 +1,5 @@
 import pymysql
+import time
 #CONNECTION TO DATABASE------------------------------------------------------------------------------------------------------
 conn = pymysql.connect(host = '127.0.0.1', user = 'root', password = 'sampo', database = 'textadventure-database')
 cur = conn.cursor()
@@ -13,7 +14,23 @@ def get_location(location_id):
         print (row[0])
         if (row[1] != ""):
             print(row[1])
-    return
+
+    print_npc(location_id)
+
+    return location_id
+
+#PRINT NPC------------------------------------------------------------------------------------------------------------------
+def print_npc(location_id):
+
+    sql2 = "SELECT locnpc_loc_id FROM locnpc WHERE locnpc_loc_id = '" + str(location_id) + "'"
+    cur.execute(sql2)
+    if cur.rowcount >= 1:
+        for row in cur.fetchall():
+            if location_id == row[0]:
+                sql = "SELECT npc.description FROM npc"
+                cur.execute(sql)
+                for row in cur:
+                    print(row[0])
 
 #LOOK WITHIN A LOCATION-------------------------------------------------------------------------------------------------------
 def look_around(location_id):
@@ -47,33 +64,120 @@ def move(location_id, direction):
         print("You cant go that way")
     return location_id
 
-def talk(location_id, answer):
+#TALK TO AN NPC-------------------------------------------------------------------------------------------------------------------
+def talk(location_id):
     cur = conn.cursor()
-    sql = "SELECT locnpc_loc_id, npc_id, line_id, line, answer_id, description FROM line INNER JOIN \
-          answer ON line.line_id = answer.answer_id WHERE answer.answer_id = '" + answer + "' AND locnpc_loc_id = '" + location_id + "' \
-           AND line_id = 1 "
 
-
+    sql = "SELECT locnpc_loc_id FROM locnpc WHERE locnpc_loc_id = '" + str(location_id) + "'"
     cur.execute(sql)
     if cur.rowcount >= 1:
         for row in cur.fetchall():
-            if location_id is row[1]:
+            if location_id == row[0]:
+                sql = "SELECT npc.name, npc.description FROM npc"
+                cur.execute(sql)
+                print("Who would you like to talk to?\n-------------------")
+                name_list = []
+                for row1 in cur:
+                    print(row1[0])
+                    name_list.append(row1[0])
 
-                while answer not in end_con:
-                    sql2 = "SELECT * FROM line WHERE line_id ="
 
 
-    return
+                print()
+                select_npc = input("--> ")
+                if len(select_npc) >= 1:
+                    select_npc[0].lower()
+
+                if select_npc in name_list:
+
+                    sql2 = "SELECT npc.name FROM npc WHERE npc.name = '" + select_npc + "'"
+                    cur.execute(sql2)
+
+
+                    sql3 = "SELECT line_id, line FROM line WHERE line_id = 1"
+                    sql4 = "SELECT previous_answer_line_id, description FROM answer INNER JOIN line ON answer.previous_answer_line_id = line.line_id WHERE answer.previous_answer_line_id = 1"
+
+                    cur.execute(sql3)
+
+                    for row3 in cur:
+                        print(select_npc + ": " + row3[1])
+
+                    print()
+                    cur.execute(sql4)
+                    #time.sleep(3)
+                    i = 0
+                    for row4 in cur:
+                        i += 1
+                        print(str(i) + ": " + row4[1])
+
+                    response = int(input("--> "))
+                    sql5 = "SELECT previous_answer_line_id, description, next_answer_line_id FROM answer WHERE previous_answer_line_id = 1"
+                    cur.execute(sql5)
+                    if cur.rowcount >= 1:
+                        row5 = cur.fetchall()
+                        if response == 1:
+                            next_line = row5[0][2]
+                        if response == 2:
+                            next_line = row5[1][2]
+                    answer(next_line, select_npc)
+
+
+
+
+
+                            #answer(response, next_line, select_npc)
+
+
+                else:
+                    print("Who?")
+                        #if (row[1] != ""):
+                         #   print(row3[1])
+
+    else:
+        print("There's no one here to talk to")
+
+def answer(next_line, select_npc):
+    sql3 = "SELECT line_id, line FROM line WHERE line_id = '" + str(next_line) + "'"
+    sql4 = "SELECT previous_answer_line_id, description FROM answer INNER JOIN line ON answer.previous_answer_line_id = line.line_id WHERE answer.previous_answer_line_id = '" + str(next_line) + "'"
+
+    cur.execute(sql3)
+
+    for row3 in cur:
+        print(select_npc + ": " + row3[1])
+
+    print()
+    cur.execute(sql4)
+    # time.sleep(3)
+    i = 0
+    for row4 in cur:
+        i += 1
+        print(str(i) + ": " + row4[1])
+
+    response = int(input("--> "))
+    sql5 = "SELECT previous_answer_line_id, description, next_answer_line_id FROM answer WHERE previous_answer_line_id = '" + str(next_line) + "'"
+    cur.execute(sql5)
+    if cur.rowcount >= 1:
+        row5 = cur.fetchall()
+        if response == 1:
+            next_line = row5[0][2]
+        if response == 2:
+            next_line = row5[1][2]
+
+
+    while row3[1] is not "1":
+        answer(next_line, select_npc)
+
 
 
 #COMMAND LIST---------------------------------------------------------------------------------------------------------------
 directions = ['n', 's', 'w', 'e', 'u', 'd', 'north', 'south', 'west', 'east', 'up', 'down']
 look = ['view', 'look', 'examine', 'inspect']
 quit = ['exit', 'quit', 'end', 'finnish']
-talk = ['talk', 'speak', 'interact']
+talk_command = ['talk', 'speak', 'interact']
 end_con = ['leave']
+bla = ['ä']
 
-all_commands = directions + look + quit
+all_commands = directions + look + quit + talk_command + end_con + bla
 
 #STARTING VALUES------------------------------------------------------------------------------------------------------------
 location_id = 1
@@ -97,7 +201,8 @@ while action not in quit:
         if answer is "y":
             break
         else:
-            action = ""
+            look_around(location_id)
+            action = "ä"
 
     #COMMAND NOT REGOGNIZED
     if action not in all_commands:
@@ -114,8 +219,8 @@ while action not in quit:
         location_id = newLocatin
 
     #INTERACTION
-    if action in talk:
-        print()
+    if action in talk_command:
+        talk(location_id)
 
     #END GAME
     if location_id is 5:
