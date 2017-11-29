8885484037d386d5c8775b7ed74b515d2bc01caa
narhimatti <mjk.narhi@gmail.com>
2017-11-29 16:08:54 +0200

    function for picking up objects

---
+++ pick.py
@@ -0,0 +1,14 @@
+def pickup(location_id, item):
+    cur=db.cursor()
+    p="UPDATE item SET item_character_id=1 WHERE item.name='"+item+"' AND pickable=TRUE"
+    s = "UPDATE item SET item_location_id=NULL WHERE item.name='"+item+"' AND item_location_id='"+ str(location_id)+ "' "
+    cur.execute(p)
+    if cur.rowcount>=1:
+        print("Taken.")
+    else:
+        print("You can't take that.")
+    cur.execute(s)
+
+pickup(2, "sword")
+pickup(1, "note")
+pickup(4, "chest")
