edc96e80a64413516cc14bf30544403607c869ae
narhimatti <mjk.narhi@gmail.com>
2017-11-30 20:08:02 +0200

    drop function edited

--- textadventure-python/drop.py
+++ textadventure-python/drop.py
@@ -1,10 +1,16 @@
-def drop(location_id, item):
-    cur=db.cursor()
-    sql="UPDATE item SET item_character_id=NULL WHERE item.name='"+item+"'"
-    sql1="UPDATE item SET item_location_id = '"+str(location_id)+"' WHERE item.name='"+item+"'"
+def drop(conn, location_id, item):
+    cur = conn.cursor()
+    if item == 'all' or item == 'everything':
+         items = "SELECT item.name FROM item WHERE item.character_id = 1"
+         sql = "UPDATE item SET item_character_id = NULL, item_location_id = '" + str(location_id) + "' WHERE item_character_id = 1"
+    else:
+        items = "SELECT item.name FROM item WHERE item.character_id = 1 AND item.name = '" + item + "' "
+        sql = "UPDATE item SET item_character_id = NULL, item_location_id = '" + str(location_id) + "' WHERE item.name = '" + item +"' AND item_character_id = 1 "
     cur.execute(sql)
-    if cur.rowcount>=1:
-        print("Dropped")
+    if cur.rowcount >= 1:
+        print("Dropped: ")
+        cur.execute(items)
+        for row in cur.fetchall():
+            print(" -" + row[0])
     else:
         print("You don't have this item")
-    cur.execute(sql1)
