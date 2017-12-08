b317e5ec5a2de49c756a76d1d597b7772210ceef
TehNinjaKiwi <miikka.virtanen3@gmail.com>
2017-12-08 21:01:51 +0200

    combat system

--- textadventure-python/combat.py
+++ textadventure-python/combat.py
@@ -1,24 +1,24 @@
 import random
 
-def damage(conn, character, npc):
-    #laitoin game.py esimerkki kutsun näille siellä näät sitten mitä arvoja tulee ja mistä tulee erroreita
+def combat(conn, location_id, npc):
+    cur = conn.cursor()
 
-    #muutin nimet selvennyksen vuoksi
-    #se miten määrität kumpi hyökkää eka voi olla vaikka jonkun arvon mukaan esim skill tai damage tai jotain
-
-    #ei tarvitse tehdä for looppia
-    #--------------------------
-    player_hp = character[0]
-    print(character[0])
-    print(character[1])
-    print(npc)
-    #-------------------------
+    sql = "SELECT hp, attack, defence, dodge, luck FROM character_"
+    cur.execute(sql)
+    character = cur.fetchall()[0]
+    print(character)
     for row in character:
         player_hp = (row[0])
         player_att = (row[1])
         player_def = (row[2])
         player_dodge = (row[3])
         player_luck = (row[4])
+
+    sql2 = "SELECT hp, attack, defence, dodge, luck FROM npc INNER JOIN location ON npc.npc_location_id = location.location_id \
+            WHERE location.location_id = '" + str(location_id) + "' AND npc.name = '" + str(npc) + "'"
+    cur.execute(sql2)
+    npc = cur.fetchall()[0]
+    print(npc)
     for row in npc:
         npc_hp = (row[0])
         npc_att = (row[1])
@@ -26,49 +26,64 @@ def damage(conn, character, npc):
         npc_dodge = (row[3])
         npc_luck = (row[4])
 
-    #sitten sun pitää lisää while loopin sisään vaihtoehdot mitä pelaaja voi tehdä
-    #hyökkää puolusta healaa pakene
-    #ja jokanen valinta kutsuu sitten omaa functiotaan
-    while player_hp > 0 or npc_hp >0: #looppaa vuoroja niin kauan kunnes toinen on kuollut
+    turn = random.randint(1, 5)
+    if turn <=3:
+        player_turn = True
+        npc_turn = False
+    else:
+        player_turn = False
+        npc_turn = True
 
-        if random.randint(player_luck, 100) > 50:
-            dealt = player_att*2
+    while player_hp > 0 or npc_hp > 0:
+
+        if player_turn:
+            command = input(int("What do you wish to do: 1 Attack, 2 Defencive formation, 3 Heal, 4 Run"))
+
+            if command == 1: #player attacks
+                if random.randint(player_luck, 100) > 80: #Checks if player strikes critically
+                    dealt = player_att*2
+                else:
+                    dealt = player_att
+                if random.randint(npc_dodge, 100) > 85: #Checks if npc dodges the attack
+                    print("The opponent dodged the attack!")
+                else:
+                    npc_hp = npc_hp - (npc_def - dealt)
+
+            if command == 2: #player goes into a defencive formation
+                player_def = player_def + 15
+                player_dodge = player_dodge + 10
+                player_att = player_att - 10
+                player_luck = player_luck +15
+
+            if command == 3: #player heals
+                player_hp = player_hp + 25
+
+            #if command == 4: #player runs away
+
         else:
-            dealt = player_att
-
-        dmg = npc_hp - (npc_def - dealt)
-
-        if random.randint(npc_dodge, 100) > 50:
-            print(npc + "dodged the attack!")
-        else:
-            #muuta tässä sql sellaseen helpommin luettavaan muotoon
-            #en nyt ymmärrä mitä haet tällä sql kyselyllä mutta etköhän sä näillä pääse vauhtiin
-            #tänne pitää myös lisää sitten se commit jotta se muuttaa arvoja tietokannassa
-            #mitä sitten kun taistelu on käyty tapahtuu?
-            sql = "UPDATE '%s' SET hp = '%d' WHERE '%s'.'%s'_id" % (npc, dmg, npc, npc)
-
-
-    return
-
-
-def combat(conn, location_id, npc):
-
-    cur = conn.cursor()
-
-    sql = "SELECT hp, attack, defence, dodge, luck FROM character_"
-    cur.execute(sql)
-    character = cur.fetchall()[0]
-    print(character)
-
-    sql2 = "SELECT hp, attack, defence, dodge, luck FROM npc INNER JOIN location ON npc.npc_location_id = location.location_id \
-            WHERE location.location_id = '" + str(location_id) + "' AND npc.name = '" + str(npc) + "'"
-    #kirjotetaan sql kyselyt tälleen. Mun mielestä paljon helpompi luke kuin sillä %s tavalla
-    cur.execute(sql2)
-    npc = cur.fetchall()[0]
-    print(npc) #hyvä tulostaa aina niin näet millasen tuloksen se antaa. Sitten tiedät mitä pitää korjata
-
-    #tässä pitää kutsua damage functiota
-    damage(conn, character, npc)
+            if npc_hp < 20:
+                print("The opponent heals!")
+                npc_hp = npc_hp + 15
+            elif random.randint(1, 5) == 1: # 20% chance that npc goes into a defencive formation
+                print("The opponent goes into a defencive formation!")
+                npc_dodge = npc_dodge + 10
+                npc_def = npc_def + 15
+                npc_att = npc_att - 5
+                npc_luck = npc_luck - 15
+            else:
+                print("The opponent attacks you!")
+                if random.randint(npc_luck, 100) > 90:
+                    dealt2 = npc_att*2
+                else:
+                    dealt2 = npc_att
+                if random.randint(player_dodge, 100) > 75:
+                    print("You dodged the attack!")
+                else:
+                    player_hp = player_hp - (player_def - dealt2)
+        player_turn = not player_turn
+        npc_turn = not npc_turn
+    sql3 = "UPDATE npc SET npc.hp = '" + npc_hp + "'  WHERE location.location_id = '" + str(location_id) + "' AND npc.name = '" + str(npc) + "'"
+    cur.execute(sql3)
+    #conn.commit()?
 
     return location_id
-
